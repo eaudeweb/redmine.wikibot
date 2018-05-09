24644667f284608c5a97f7bd71df3d943f07119b
Andra Necula <andra.easypost@gmail.com>
2018-05-09 15:49:26 +0300

    script

---
+++ listcontainers.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+import json
+import urllib, urllib2, os, sys, logging, time
+import ConfigParser, getopt
+from operator import itemgetter, attrgetter, methodcaller
+
+from redmine import Redmine
+
+class Discover(object):
+
+    containers = {}
+    hosts = {}
+
+    def __init__(self, config):
+        self.config = config
+
+    def write_page(self, content):
+        server = self.config.get('wiki','server')
+        apikey = self.config.get('wiki','apikey')
+        projectName = self.config.get('wiki','project')
+        pageName = self.config.get('wiki','containerpage')
+        #pageTitle = self.config.get('wiki','title')
+        server = Redmine(server, key=apikey, requests={'verify': True})
+        project = server.project.get(projectName)
+
+        try:
+            wiki_page = server.wiki_page.get(pageName, project_id=project.id)
+            server.wiki_page.update(pageName, project_id=project.id, text=content)
+        except ResourceNotFoundError:
+            wiki_page = server.wiki_page.create(project_id=project.id, title=pageName, text=content)
+    
+    def write_stdout(self, content):
+        #pass
+        print (content)
+
+    def get_operation(self, environment, url):
+        rancherUrl = self.config.get(environment, "rancher_url", vars={'env': environment})
+        rancherAccessKey = self.config.get(environment, "rancher_access_key")
+        rancherSecretKey = self.config.get(environment, "rancher_secret_key")
+        realm = "Enter API access key and secret key as username and password"
+
+        auth_handler = urllib2.HTTPBasicAuthHandler()
+        auth_handler.add_password(realm=realm, uri=rancherUrl, user=rancherAccessKey, passwd=rancherSecretKey)
+        opener = urllib2.build_opener(auth_handler)
+        urllib2.install_opener(opener)
+        f = urllib2.urlopen(url)
+        rawdata = f.read()
+        f.close()
+        return json.loads(rawdata)
+
+    def load_containers(self, environment):
+        self.containers = {}
+        rancherUrl = config.get(environment, "rancher_url", vars={'env': environment})
+        structdata = self.get_operation(environment, rancherUrl + "/containers?limit=1000")
+        for instance in structdata['data']:
+            imageUuid = instance['imageUuid']
+            if imageUuid.startswith("docker:rancher/") and not imageUuid.startswith("docker:rancher/lb-service-haproxy"): continue
+            imageUuid = imageUuid[7:]
+            containerStruct = self.containers.setdefault(imageUuid, [])
+            containerStruct.append(instance)
+            #print instance
+
+    def load_hosts(self, environment):
+        self.hosts = {}
+        rancherUrl = config.get(environment, "rancher_url", vars={'env': environment})
+        structdata = self.get_operation(environment, rancherUrl + "/hosts")
+        for instance in structdata['data']:
+            name = instance['hostname']
+            hostId = instance['id']
+            self.hosts[hostId] = name
+            self.containers[hostId] = []
+        #print self.hosts
+
+    def buildgraph(self, content):
+        content.append('|_. Image |_. Container |_. State |_. MemLimit |')
+        for imageName, containers in sorted(self.containers.items()):
+            #content.append('h3. {}\n'.format(imageName))
+            for container in sorted(containers, key=itemgetter('name')):
+#               if container['imageUuid'].startswith("docker:rancher/"): continue
+                memoryRes = container.get('memory', 0)
+                if memoryRes is None: memoryRes = 0
+                memoryRes = memoryRes / 1048576
+                content.append('| {} | {} | {} |>. {} |'.format(imageName, container['name'], container['state'], memoryRes))
+#           content.append('\n')
+
+if __name__ == '__main__':
+    config = ConfigParser.SafeConfigParser()
+    config.read('rancher.cfg')
+    dryrun = False
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "vn")
+    except getopt.GetoptError, err:
+        sys.exit(2)
+    for o, a in opts:
+        if o == "-v":
+            logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+        if o == "-n":
+            dryrun = True
+    if len(args) > 0:
+        environments = args
+    else:
+        environments = config.get("general", "environments").split()
+    pageTitle = config.get('wiki','containertitle')
+    content = []
+    content.append('h1. ' + pageTitle + '\n\n')
+    content.append('Automatically discovered on ' + time.strftime('%d %B %Y') + '. _Do not update this page manually._')
+
+    for environment in environments:
+        #print environment
+        logging.debug("Environment: %s", environment)
+        envLabel = config.get(environment, 'label')
+        content.append('\nh2. Environment: {}\n'.format(envLabel))
+        content.append('\n')
+        disc = Discover(config)
+#       disc.load_hosts(environment)
+        disc.load_containers(environment)
+        disc.buildgraph(content)
+        content.append('\n')
+    if dryrun:
+        disc.write_stdout("\n".join(content))
+    else:
+        disc.write_page("\n".join(content))
+    #print "Done"
